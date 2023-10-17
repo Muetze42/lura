cce866486914d68cf50a686fc7f5f4b03a08bac2
Norman Huth <norman@huth.it>
2023-10-17 15:07:11 +0200

    feat: add addDependency method

--- src/LuraInstaller.php
+++ src/LuraInstaller.php
@@ -2,6 +2,8 @@
 
 namespace NormanHuth\ConsoleApp;
 
+use NormanHuth\Helpers\Str;
+
 abstract class LuraInstaller
 {
     /**
@@ -10,4 +12,68 @@ abstract class LuraInstaller
      * @param mixed|\NormanHuth\ConsoleApp\LuraCommand $command
      */
     abstract public function runLura(mixed $command);
+
+    /**
+     * Collected dependencies version from https://github.com/Muetze42/data.
+     *
+     * @var array{composer: array<array-key, string>, npm: array<array-key, string>}|null
+     */
+    protected static ?array $dependenciesVersions = null;
+
+    /**
+     * Add a dependencies to json.
+     *
+     * @param array  $dependencies
+     * @param string $package
+     * @param string $version
+     * @param bool   $updateVersion
+     *
+     * @return void
+     */
+    protected static function addDependency(
+        array &$dependencies,
+        string $package,
+        string $version,
+        bool $updateVersion = true
+    ): void {
+        if ($updateVersion) {
+            $versions = static::getDependenciesVersions();
+
+            $version = data_get(
+                $versions,
+                'composer.' . $package,
+                data_get(
+                    $versions,
+                    'npm.' . $package,
+                    $version
+                )
+            );
+        }
+
+        $dependencies = array_merge($dependencies, [$package => $version]);
+        ksort($dependencies, SORT_NATURAL | SORT_FLAG_CASE);
+    }
+
+    /**
+     * @return array
+     */
+    protected static function getDependenciesVersions(): array
+    {
+        if (is_null(static::$dependenciesVersions)) {
+            if (
+                $contents = file_get_contents(
+                    'https://raw.githubusercontent.com/Muetze42/data/main/storage/versions.json'
+                )
+            ) {
+                if (Str::isJson($contents)) {
+                    static::$dependenciesVersions = json_decode($contents, true);
+                }
+            }
+        }
+        if (is_null(static::$dependenciesVersions)) {
+            static::$dependenciesVersions = [];
+        }
+
+        return static::$dependenciesVersions;
+    }
 }
